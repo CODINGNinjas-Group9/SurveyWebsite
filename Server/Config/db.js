4e72c3ed42b612ceef6947ff320d65a59cafdd46
NilamKeshwala <nilamkeshwala2000@gmail.com>
2021-07-15 14:10:41 -0400

    DB connectivity
    DB connectivity update

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
-exports.LocalURI = "mongodb://localhost/clothing_store";
+exports.HostName = exports.RemoteURI = void 0;
 exports.RemoteURI = "mongodb+srv://user1:YgASTsP5iQFQCmoX@clusterforsurveywebsite.0zu4s.mongodb.net/surveywebsite_db?retryWrites=true&w=majority";
 exports.HostName = "Remotehost";
 //# sourceMappingURL=db.js.map
