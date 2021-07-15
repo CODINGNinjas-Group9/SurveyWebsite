a48020032af75ef6c759c87ba6a9da9c54a471a3
CODINGNinjas-Group9 <87380335+CODINGNinjas-Group9@users.noreply.github.com>
2021-07-15 16:47:01 -0400

    ejs file for MCQ Template

---
+++ .history/Server/Controllers/index_20210715164614.ts
@@ -0,0 +1,82 @@
+import express, { Request, Response, NextFunction } from "express";
+import Survey from "../Models/Survey";
+
+// Display Page Functions
+
+export function DisplayHomePage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.render("index", { title: "Home", page: "home" });
+}
+
+export function DisplayCreateSurveysPage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.render("index", { title: "Create Survey", page: "createSurvey" });
+}
+
+export function ProcessCreateSurveysPage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  let newSurvey = new Survey({
+    title: req.body.surveytitle,
+    validDate: req.body.validity,
+    description: req.body.description,
+    creator: "Group-9",
+    questions: {
+      q1: { questionText: req.body.q1 },
+      q2: { questionText: req.body.q2 },
+      q3: { questionText: req.body.q3 },
+      q4: { questionText: req.body.q4 },
+      q5: { questionText: req.body.q5 },
+    },
+  });
+  Survey.create(newSurvey, (err) => {
+    if (err) {
+      console.log(err);
+      return res.end(err);
+    }
+  });
+  res.redirect("/");
+}
+
+export function DisplayAvailableSurveysPage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.render("index", { title: "Available Surveys", page: "availablesurveys" });
+}
+
+export function DisplayLoginPage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.render("index", { title: "Login", page: "login" });
+}
+
+export function DisplaySignupPage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.render("index", { title: "Sign Up", page: "signup" });
+}
+
+export function DisplayCreateSurveyTemplatePage(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.render("index", {
+    title: "Create Survey",
+    page: "createSurveyTemplate",
+  });
+}
