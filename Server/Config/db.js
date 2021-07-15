76bda6b57a832c4919547c41faab847d6ba8ad9c
Muhammad Hassan <hassaniq@live.com>
2021-07-14 20:41:53 -0400

    Changed database name

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/clothing_store";
-exports.RemoteURI = "mongodb+srv://user1:YgASTsP5iQFQCmoX@clusterforsurveywebsite.0zu4s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://user1:YgASTsP5iQFQCmoX@clusterforsurveywebsite.0zu4s.mongodb.net/surveywebsite_db?retryWrites=true&w=majority";
 exports.HostName = "Remotehost";
 //# sourceMappingURL=db.js.map
